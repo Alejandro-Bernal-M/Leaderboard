743e78474f9861d9fe66fb7fe646a6c08c9123fa
alejandro <alejandrober96@gmail.com>
2023-03-08 16:58:16 -0500

    Functionality for the construction of all the list elements

---
+++ src/modules/refreshHTML.js
@@ -0,0 +1,11 @@
+import getScore from './getScore.js';
+import constructHTML from './constructHTML.js';
+const scoreHolder = document.querySelectorAll('.li-scores');
+
+export default () => {
+  for (let i = 0; i < scoreHolder.length; i += 1) {
+    getScore().then((res) => {
+      constructHTML(res[i], scoreHolder[i]);
+    });
+  }
+};
