6e3e967fe3bc97d03d83ae8507ae8e2f37a745be
alejandro <alejandrober96@gmail.com>
2023-03-08 17:02:32 -0500

    Improve on refreshing after adding score at index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', (e) => {
   const score = document.querySelector('.score');
   setScore(name.value, parseInt(score.value, 10));
   form.reset();
-  refreshHTML();
+  setTimeout(refreshHTML(), 1500);
   msg.innerHTML = 'Score saved';
   setTimeout(() => {
     msg.innerHTML = '';
