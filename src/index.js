51cb5eb1ab8d602d410c1c825455a46ab36b15b3
alejandro <alejandrober96@gmail.com>
2023-03-08 16:58:48 -0500

    Implementing all the functionality together

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles.css';
+import setScore from './modules/setScore.js';
+import refreshHTML from './modules/refreshHTML.js';
+
+
+const form = document.querySelector('.form');
+const btnRefresh = document.querySelector('.btn-refresh');
+const msg = document.querySelector('.msg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('.name');
+  const score = document.querySelector('.score');
+  setScore(name.value, parseInt(score.value, 10))
+  form.reset();
+  refreshHTML();
+  msg.innerHTML= 'Score saved';
+  setTimeout(() => {
+    msg.innerHTML= '';
+  }, 4000);
+})
+
+btnRefresh.onclick = () => refreshHTML();
+window.onload =() => refreshHTML();
