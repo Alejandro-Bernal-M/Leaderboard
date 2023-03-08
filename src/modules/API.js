a9cf6a488c2a24a1ad1e744cb2dc791d13e5e0f9
alejandro <alejandrober96@gmail.com>
2023-03-08 16:53:51 -0500

    Creation of game on the API

---
+++ src/modules/API.js
@@ -0,0 +1,21 @@
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const game = { 
+	name: 'My leaderboard new game', 
+};
+
+const config = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(game),
+};
+
+async function setGame() {
+ const response = fetch(apiUrl, config).then((response) => response.json());
+ return response;
+}
+
+export default setGame;
+
+
